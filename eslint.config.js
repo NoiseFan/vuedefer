608ca0bd1f18d429a8e2a1555b8bed65b149548c
noise <noisefan@163.com>
2026-02-03 15:35:46 +0800

    test: add test config & compent test case - Test logic for slot rendering, transition animations, and scroll triggers

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,14 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   vue: true,
+  test: {
+    overrides: {
+      'test/padding-around-after-all-blocks': 'error',
+      'test/padding-around-after-each-blocks': 'error',
+      'test/padding-around-before-all-blocks': 'error',
+      'test/padding-around-before-each-blocks': 'error',
+      'test/padding-around-describe-blocks': 'error',
+      'test/padding-around-test-blocks': 'error',
+    },
+  },
 })
--- tests/lazy-render.browser.spec.ts
+++ tests/lazy-render.browser.spec.ts
@@ -1,7 +1,13 @@
+import type { VNode } from 'vue'
 import { describe, expect, it } from 'vitest'
 import { render } from 'vitest-browser-vue'
-import { h } from 'vue'
+import { h, nextTick, ref, Transition } from 'vue'
 import { LazyRender } from '../src/lazy-render'
+
+const DefaultText = 'Hello world!'
+const FallbackText = 'Live!'
+const DefaultComp = () => h('div', DefaultText)
+const FallbackComp = () => h('p', FallbackText)
 
 describe('lazy-render', () => {
   it('component is defined', () => {
@@ -12,28 +18,38 @@ describe('lazy-render', () => {
     expect(LazyRender.name).toBe('LazyRender')
   })
 
-  describe('element not in viewport', () => {
-    describe('porps', () => {
-      it('default tag is div', () => {
-        const Comp = render(() => h(LazyRender, null, { fallback: () => h('p', 'Hello world!') }))
-        expect(Comp.container.firstChild?.nodeName).toBe('DIV')
+  it('slot render', async () => {
+    const { getByText } = render(() => createComponent(10))
 
-        expect(Comp.getByText('Hello world!')).not.toBeInViewport()
-      })
+    await expect.element(getByText(DefaultText)).not.toBeInTheDocument()
+    await expect.element(getByText(FallbackText)).toBeInTheDocument()
 
-      it('custom tag works', () => {
-        const Comp = render(LazyRender, { props: { tag: 'section' } })
-        expect(Comp.container.firstChild?.nodeName).toBe('SECTION')
-      })
+    window.scrollTo(0, 200)
+    await expect.element(getByText(DefaultText)).toBeInTheDocument()
+    await expect.element(getByText(FallbackText)).not.toBeInTheDocument()
 
-      it('slot is work', async () => {
-        const { getByText } = render(() => h(LazyRender, null, { fallback: () => h('p', 'Live!') }))
-        await expect.element(getByText('Live!')).toBeInViewport()
-      })
+    window.scrollTo(0, 0)
+    await expect.element(getByText(DefaultText)).toBeInTheDocument()
+    await expect.element(getByText(FallbackText)).not.toBeInTheDocument()
+  })
+
+  describe('use vue\'s components wrapper', () => {
+    it('transition ', async () => {
+      const flag = ref(true)
+      const { getByText } = render(() => h(Transition, null, () => flag.value ? [] : createComponent(1)))
+      await expect.element(getByText(DefaultText)).not.toBeInTheDocument()
+
+      flag.value = false
+      await nextTick()
+      await expect.element(getByText(FallbackText)).toBeInTheDocument()
     })
   })
-
-  it('demo', () => {
-    const Comp = render(LazyRender)
-  })
 })
+
+function createDOMElements(length: number): Array<VNode> {
+  return Array.from({ length }).map((_, index) => h('div', { style: { height: '100px' } }, `Item ${index + 1}`))
+}
+
+function createComponent(length: number, tag: string = 'section'): VNode {
+  return h(tag, [...createDOMElements(length), h(LazyRender, null, { default: DefaultComp, fallback: FallbackComp })])
+}
